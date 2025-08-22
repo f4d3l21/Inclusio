6ecf27134b5ff1979636168a55c5f80c2ad8ff4c
f4d3l21 <fadel-du-21@hotmail.fr>
2025-08-22 01:55:06 +0100

    ✅ Fix ESLint & Wave audit CI/CD

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+// eslint.config.js
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module"
+    },
+    rules: {
+      // tes règles ici
+    }
+  }
+]
